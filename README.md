d19cb4557c6881443b25d364818f85309af2ef66
Keano Brion <keanobrion@gmail.com>
2021-02-02 08:38:10 +0800

    Added a new p element on the screen which displays the numbers and operator that were evaluated; Also, modified the screen elements to accomodate longer number input. Added an if function to prepend the screen with a 0 when clicking the decimal button whenever necessary.; Added a README.md

---
+++ README.md
@@ -0,0 +1,10 @@
+This calculator project is part of The Odin Project Foundations Curriculum.
+
+The calculator design and functionality was inspired by the default calculator app of my phone. With this, I did my best to copy the functionalities that are present in my phone's calculator.
+
+The project is still work in progress and I was hoping to be able to implement the same functionalities with a much precise/cleaner code. Hopefully, I would be able to improve my coding skills to shorten/simplify the codes/function that I have used in this project.
+
+In order to make the calculator work, I have included 'Indicators' which are stored in various variables with the values 0 or 1. These indicators allow the calculator to operate for the following scenarios:
+1. Indicator to let the calculator initiate a screen clear upon input of the second Number for the operation.
+2. Indicator to let the calculator initiate an operation when an operator is clicked instead of a the equal sign. ex. 12 + 7 - 5 * 3 = 42
+3. Indicator to let the calculator initiante an operation even when the equal sign is pressed more than once. ex. 1+1 = = = should be equal 4. Indicator to avoid the calculator to input more than one decimal point for a number; and prepend the number with "0" whenever necessary.
--- script.js
+++ script.js
@@ -1,82 +1,133 @@
 let firstNumber;
 let secondNumber;
-let operand;
-let i =0; //indicator for the secondNumber
-let clearIndicator =1; //clear the values Indicator
-let decimalIndicator =1; //indicator to allow input of decimal
-let operandIndicator = 0;
-
-let screen = document.querySelector('#screen p');
-screen.textContent="";
+let operator;
+let i =0; //Indicator if screen should be cleared ex. screen should be cleared when typing secondNumber
+let clearIndicator =1;  //Indicator for the equals button; such that the operation would repeat when equal button is pressed more than once ex. 1+1 = = = should be equal 4 
+let decimalIndicator =1; //Indicator to prevent two or more decimal points;
+let operatorIndicator = 0; //Indicator to allow operator button function for several operations ex. 12 + 7 - 5 * 3 = 42
+let screen = document.querySelector('#screen #operation');
+let equation  = document.querySelector('#screen #equation');
+let operatorButton = Array.from(document.querySelectorAll('#buttons .operators'));
 let buttons = Array.from(document.querySelectorAll('#buttons div'));
 
-let operatorButton = Array.from(document.querySelectorAll('#buttons .operators'));
+screen.textContent="";
+equation.textContent="";
+
 operatorButton.forEach(button => button.addEventListener('click', store));
-
-
 document.querySelector('#equals').addEventListener('click', equals);
 document.querySelector('#clear').addEventListener('click', clear);
 document.querySelector('#decimal').addEventListener('click', decimal);
 document.querySelector('#plusMinus').addEventListener('click', plusMinus);
 document.querySelector('#delete').addEventListener('click',backSpace);
-document.addEventListener('keydown', inputScreen); //input
+document.addEventListener('keydown', inputScreen); 
 
 
-function store(e){
-    if(operandIndicator ===0){
+function operatorStore(e){
         clearIndicator =1;
         decimalIndicator = 1;
-        operand = e.target.getAttribute('id');
-        console.log(operand);
+        operator = e.target.getAttribute('id');
         if(screen.textContent===""){
             firstNumber = 0;
         }
         else {
             firstNumber = parseFloat(screen.textContent);
         }
-        operandIndicator = 1;
+        operatorIndicator = 1;
+}
+
+function store(e){
+    if(operatorIndicator ===0){
+        operatorStore(e)
+        operatorIndicator = 1;
     }
     else{
         equal();
-        clearIndicator =1;
-        decimalIndicator = 1;
-        operand = e.target.getAttribute('id');
-        console.log(operand);
-        if(screen.textContent===""){
-            firstNumber = 0;
-        }
-        else {
-            firstNumber = parseFloat(screen.textContent);
-        }
+        operatorStore(e);
     }
 }
 
-
-function equals(){
-    equal();
-    operandIndicator=0;
-}
-
+//function when operator act as an equal operator ex. 12 + 7 - 5 * 3 =
 function equal(){
+    if(firstNumber === undefined) return;
     if (clearIndicator == 1){
     secondNumber = parseFloat(screen.textContent);
-    screen.textContent=operate(firstNumber, secondNumber, operand);
+    screen.textContent=operate(firstNumber, secondNumber, operator);
     clearIndicator = 0;
     decimalIndicator = 1;
     i=0; 
     }
     else{
-    firstNumber = operate(firstNumber, secondNumber, operand);
-    screen.textContent=operate(firstNumber, secondNumber, operand);
+    firstNumber = operate(firstNumber, secondNumber, operator);
+    screen.textContent=operate(firstNumber, secondNumber, operator);
     i=0;
     }
+    equation.textContent=`${firstNumber} ${convertOperator(operator)} ${secondNumber} =`;
 }
 
+//function when equals button is pressed
+function equals(){
+    equal();
+    operatorIndicator=0;
+}
 
-function operate(firstNumber, secondNumber, operand){
+function clear(e){
+    firstNumber = undefined;
+    secondNumber = undefined;
+    operator = undefined;
+    clearIndicator = 1;
+    screen.textContent="";
+    equation.textContent="";
+}
+
+function decimal(e){
+    if(decimalIndicator === 1){
+        if(operator!=undefined){
+            switch(i){
+                case 0:
+                    screen.textContent = "0";
+                    screen.textContent += '.';
+                    i += 1;
+                    decimalIndicator = 0;
+                    break;
+                case 1:
+                    screen.textContent += '.';
+                    decimalIndicator = 0;
+                    break;
+            }
+        }
+        else{
+            if(screen.textContent===""){
+                screen.textContent += "0."
+                decimalIndicator = 0;
+            }
+            else{
+                screen.textContent += "."
+                decimalIndicator = 0;
+            }
+        }
+
+
+    }
+    else return;
+}
+
+function plusMinus(e){
+    screen.textContent = parseFloat(screen.textContent)*-1;
+}
+
+function backSpace(e){
+    let array = screen.textContent.split("")
+    if (array.length >0){
+        array.pop();
+        screen.textContent = array.join("");
+    }
+    else return;
+}
+
+function operate(firstNumber, secondNumber, operator){
     console.log(firstNumber);
     let result;
-    switch(operand){
+    switch(operator){
         case 'add':
             result = add(firstNumber,secondNumber);
             break;
@@ -112,34 +163,12 @@ function division(a,b){
     else return Math.round((a/b)*100000)/100000;
 }
 
-function clear(e){
-    firstNumber = undefined;
-    secondNumber = undefined;
-    operand = undefined;
-    clearIndicator = 1;
-    screen.textContent="";
-}
-
-function decimal(e){
-    if(decimalIndicator === 1){
-    screen.textContent += "."
-    decimalIndicator = 0;
-    }
-    else return;
-}
-
-function plusMinus(e){
-    screen.textContent = parseFloat(screen.textContent)*-1;
-}
-
-function backSpace(e){
-    let array = screen.textContent.split("")
-    if (array.length >0){
-        array.pop();
-        screen.textContent = array.join("");
-    }
-    else return;
-}
+function convertOperator(operator) {
+    if (operator === "division") return "÷";
+    if (operator  === "multiply") return "×";
+    if (operator  === "subtract") return "−";
+    if (operator  === "add") return "+";
+  }
 
 buttons.forEach(buttons => buttons.addEventListener('click', selected));
 
@@ -154,8 +183,8 @@ function selected(e){
     }
     else if (typeButton.includes('numbers')){
         e.target.classList.add('selectedNumbers');
-        console.log(operand);
-        if(operand!=undefined){
+        console.log(operator);
+        if(operator!=undefined){
             switch(i){
                 case 0:
                     screen.textContent = "";
@@ -200,7 +229,7 @@ function inputScreen(e){
         console.log(String.fromCharCode(x));
         let y = String.fromCharCode(x);
         document.querySelector(`div[data-number="${y}"]`).classList.add('selectedNumbers');
-        if(operand!=undefined){
+        if(operator!=undefined){
             switch(i){
                 case 0:
                     screen.textContent = "";
@@ -219,7 +248,5 @@ function inputScreen(e){
 if(x===27){
     clear();
 }
-    
-    
-
 }
+
